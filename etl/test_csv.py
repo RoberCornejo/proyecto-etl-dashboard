99f42f8ca411875df6543898d119a85760ea1773
Roberto Alejandro Cornejo <roberto.a.cornejoj@gmail.com>
2026-06-19 20:02:28 -0400

    feat: integración API OMDb funcionando + validación completa pipeline

--- etl/test_csv.py
+++ etl/test_csv.py
@@ -1,6 +1,11 @@
 from transform import clean_movies_data
 from extract import get_movies_for_api
 
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from api.api import get_movie_data
 
 file_path = "data/netflix_titles.csv"
 
@@ -14,3 +19,9 @@ movies = get_movies_for_api(df)
 
 print("\n✅ Películas para API:")
 print(movies)
+
+# ✅ Test API
+print("\n✅ Test conexión API:")
+
+result = get_movie_data("Titanic", 1997)
+print(result)
